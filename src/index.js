eaa7c30cea5ac7bfb0143a08732a87cda86ea9bd
Jason Hsu <jason80d@gmail.com>
2017-05-09 13:58:49 +0800

    chore: construct the redux construction

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { Router, Route, browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+
+import 'antd/dist/antd.less';
+
+import configureStore from './store/configureStore';
 import App from './App';
 
+const store = configureStore();
+const history = syncHistoryWithStore(browserHistory, store);
+
+const routerSet = () => {
+  return (
+    <Router history={history}>
+      <Route path="/" component={App} />
+    </Router>
+  );
+};
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    {routerSet()}
+  </Provider>,
   document.getElementById('root'),
 );
